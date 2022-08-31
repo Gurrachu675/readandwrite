e8f6749bc3818471e95c1f5e668aee11ef44bce4
Gurrachu675 <pepethefrog675@gmail.com>
2022-08-31 14:08:45 +0200

    added namspace SusImposter
    template print function

--- readandwrite/src/main.cpp
+++ readandwrite/src/main.cpp
@@ -1,4 +1,20 @@
+#include<iostream>
+
+
+namespace SusImposter 
+{
+	template <typename T>
+	void print(T t)
+	{
+		std::cout << t << "\n";
+	}
+
+}
+
+
+
+
 int main()
 {
-
+	SusImposter::print(123);
 }
